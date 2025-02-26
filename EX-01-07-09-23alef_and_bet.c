a33b635289bc1732be3d4c7ffe91cac09c5b9fb7
yotamgr314 <yotamgr314@gmail.com>
2025-02-26 23:10:29 +0200

    asdsad
    asd

--- EX-01-07-09-23alef_and_bet.c
+++ EX-01-07-09-23alef_and_bet.c
@@ -15,24 +15,30 @@ void cleanup_mutex(void* arg)
     pthread_mutex_unlock((pthread_mutex_t*)arg);
 }
 
-void cleanup_mem(void* arg)
-{
-    free()
+void cleanup_word(void *arg) {
+    if (p != NULL) {
+        free(p);
+        p = NULL;
+    }
 }
 
-pthread_cond_t qcond = PTHREAD_COND_INITIALIZER;
 
+pthread_cond_t qcond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t qlock = PTHREAD_MUTEX_INITIALIZER;
 create_word(char*s)
 {
+    pthread_mutex_lock(&qlock)
     pthread_cleanup_push(pthread_mutex_unlock, &qlock);
     if(p == NULL)
     p=(char*)malloc(1000);
-
-    pthread_mutex_lock(&qlock);
+    pthread_clean_up_push(cleanup_mem, p);
+    
     strcpy(p,s);
     pthread_mutex_unlock(&qlock);
 
 
     pthread_cleanup_pop(0);
+    pthread_cleanup_pop(1);
+
 
 }
